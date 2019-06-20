cc43142e60c3c2f4f3f123ce9bcdc23bf7332cf1
Robert Tam <rsh.tam@gmail.com>
2019-06-20 18:47:37 +0100

    Added some css

--- js/app.js
+++ js/app.js
@@ -16,23 +16,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const category = evt.target.category.value;
 
     if (document.querySelectorAll('ul').length === 0) {
-      console.log("hi");
       const deleteButton = document.createElement("button");
       const deleteText = document.createTextNode("Delete All");
       deleteButton.appendChild(deleteText);
-      deleteButton.addEventListener("click", handleDeleteButton)
+      deleteButton.addEventListener("click", handleDeleteButton);
+      deleteButton.classList.add("btn");
       reading_List.appendChild(deleteButton);
     }
 
     const newList = document.createElement("ul");
     let newListItem = document.createElement("li");
     newListItem.textContent = `Title: ${title}`;
+    newListItem.classList.add("title");
     newList.appendChild(newListItem);
     newListItem = document.createElement("li");
     newListItem.textContent = `Author: ${author}`;
+    newListItem.classList.add("author");
     newList.appendChild(newListItem);
     newListItem = document.createElement("li");
     newListItem.textContent = `Category: ${category}`;
+    newListItem.classList.add("category");
     newList.appendChild(newListItem);
     reading_List.appendChild(newList);
 
@@ -41,6 +44,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector("#new-item-form");
   form.addEventListener("submit", handleFormSubmit);
-
 
 })
